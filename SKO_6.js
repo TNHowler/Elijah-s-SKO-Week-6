5eace2343ed412b87400aef42aef935fd02ef091
Elijah <s3945008@student.rmit.edu.au>
2024-10-28 11:05:37 +1100

    Add files via upload

--- SKO_6.js
+++ SKO_6.js
@@ -1,8 +1,14 @@
 var elevator;
 var ellipses = [];
+let InterstateBCFont;
+let DrawingCode;
+let Table;
 
 function preload(){
   elevator = loadImage("data/Elevator.jpeg");
+  InterstateBCFont = loadFont("Interstate_BoldCondensed.ttf");
+  DrawingCode = loadImage("data/Drawing_Code.png");
+  Table = loadImage("data/Table.png");
 }
 
 function setup() {
@@ -12,23 +18,31 @@ function setup() {
     { x: width / 1.1321, y: height / 1.617, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-1/" },
     { x: width / 1.1321, y: height / 1.789, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-3/" },
     { x: width / 1.1321, y: height / 2, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-5/" },
-    { x: width / 1.1321, y: height / 2.27, w: width / 40, h: height / 22, url: "https://example.com/5" },
-    { x: width / 1.1321, y: height / 2.625, w: width / 40, h: height / 22, url: "https://example.com/6" },
-    { x: width / 1.1321, y: height / 3.11, w: width / 40, h: height / 22, url: "https://example.com/7" },
+    { x: width / 1.1321, y: height / 2.27, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-7/" },
+    { x: width / 1.1321, y: height / 2.625, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-9/" },
+    { x: width / 1.1321, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-11/" },
 
     { x: width / 1.0925, y: height / 1.475, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-0/" },
     { x: width / 1.0925, y: height / 1.617, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-2/" },
     { x: width / 1.0925, y: height / 1.789, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-4/" },
-    { x: width / 1.0925, y: height / 2, w: width / 40, h: height / 22, url: "https://example.com/11" },
-    { x: width / 1.0925, y: height / 2.27, w: width / 40, h: height / 22, url: "https://example.com/12" },
-    { x: width / 1.0925, y: height / 2.625, w: width / 40, h: height / 22, url: "https://example.com/13" },
-    { x: width / 1.0925, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/The-Zipped-Face-Final/" }
+    { x: width / 1.0925, y: height / 2, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-6/" },
+    { x: width / 1.0925, y: height / 2.27, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-8/" },
+    { x: width / 1.0925, y: height / 2.625, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-10/" },
+    { x: width / 1.0925, y: height / 3.11, w: width / 40, h: height / 22, url: "https://tnhowler.github.io/Elijah-s-SKO-Week-12/" },
   ];
 }
 
 function draw() {
   background("white");
   image(elevator, 0, 0, windowWidth, windowHeight);
+
+  imageMode(CENTER);
+
+  image(Table, width / 2.65, height / 1.3, width / 4, height / 4);
+
+  image(DrawingCode, width / 1.6, height / 1.3, width / 5, height / 4);
+
+  imageMode(CORNER);
 
   strokeWeight(width / 550);
   noFill();
@@ -44,6 +58,33 @@ function draw() {
     }
     ellipse(e.x, e.y, e.w, e.h);
   }
+
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth / 50);
+  textFont(InterstateBCFont); 
+  noStroke();
+  fill(0);
+  text(
+    "Codewords Creative Coding and Typography",
+    width / 2.5,
+    height / 10,
+    width / 5, // Specify the width for wrapping the text
+    height / 12,  // Specify the height for wrapping the text
+  );
+
+  // Display text in the middle of the screen
+  textAlign(CENTER, CENTER);
+  textSize(windowWidth / 115);
+  textFont(InterstateBCFont); 
+  noStroke();
+  fill(0); 
+  text(
+    "Over the week break we were required to edit, refine or completely change our paper prototypes and create a storyboard like sequence of how our projects. On the SKO side of things we needed to draw a simple demonstration of how the website will be used. \n\nIn class we had a refresh of our tables projects, to see what minor or huge changes we made to our projects. \n\nIn the afternoon class we worked on creating tables with HTML and adding images and text to the sections of the table. We also did a little bit of work in p5 where an image was placed on the screen and the mouse would leave a trail of the image. \n\nClick on the code to draw with my dog!",
+    width / 2.65,
+    height / 5,
+    width / 4, // Specify the width for wrapping the text
+    height / 4  // Specify the height for wrapping the text
+  );
 }
 
 function mousePressed() {
@@ -54,4 +95,12 @@ function mousePressed() {
       window.location.href = e.url;
     }
   }
+
+  let DrawingCodeX = width / 1.6;  
+  let DrawingCodeY = height / 1.3;  
+  let DrawingCodeW = width / 5;  
+  let DrawingCodeH = height / 4;  
+  if (mouseX > DrawingCodeX - DrawingCodeW / 2 && mouseX < DrawingCodeX + DrawingCodeW / 2 && mouseY > DrawingCodeY - DrawingCodeH / 2 && mouseY < DrawingCodeY + DrawingCodeH / 2) {  
+   window.location.href = "https://tnhowler.github.io/Draw-Boba/";
+  }  
 }
